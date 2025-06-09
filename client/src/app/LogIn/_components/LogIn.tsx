cef44d6e890357f429ebe30922cc9c62867f3c73
baabar02 <baabar02githu.com>
2025-06-09 12:32:40 +0800

    ads

--- client/src/app/LogIn/_components/LogIn.tsx
+++ client/src/app/LogIn/_components/LogIn.tsx
@@ -87,7 +87,7 @@ export const LogIn = ({
           <div className="flex justify-between">
             <Button
               variant="link"
-              // onClick={nextStep}
+              // onClick={()=>router.push("/forgot")}
               className="text-blue-600 underline"
             >
               Forgot password?
