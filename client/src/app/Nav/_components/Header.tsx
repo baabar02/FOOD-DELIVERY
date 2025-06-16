9c043c9a07c9004d456c29f8f1150dacc2a546a0
baabar02 <baabar02githu.com>
2025-06-16 12:41:41 +0800

    ads

--- client/src/app/Nav/_components/Header.tsx
+++ client/src/app/Nav/_components/Header.tsx
@@ -1,38 +1,64 @@
-import  Image  from "next/image"
-import { Input } from "@/components/ui/input"
-import { ChevronRight, MapPin, Pin, ShoppingCart, User } from "lucide-react"
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { ChevronRight, MapPin, Pin, ShoppingCart, User } from "lucide-react";
 
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/app/UserProvider";
 
+export const Header = () => {
+  const { user, setUser } = useAuth();
+  const router = useRouter();
 
-export const Header = () =>{
-    return (
-        <div className="flex w-full h-[172px] bg-[#18181B] mx-auto ">
-            <div className="mx-auto px-4 flex gap-4">
-              <Image
-            alt="Food Delivery App Logo"
-            width={38}
-            height={46}
-            src="/Logo.png"
-            priority
-            className="object-contain"
-          />
-          <Image 
-          src="/Text.png" 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    router.push("/LogIn");
+  };
+
+  return (
+    <div className="flex w-full h-[172px] bg-[#18181B] mx-auto ">
+      <div className="mx-auto px-4 flex gap-4">
+        <Image
+          alt="Food Delivery App Logo"
+          width={38}
+          height={46}
+          src="/Logo.png"
+          priority
+          className="object-contain"
+        />
+        <Image
+          src="/Text.png"
           alt="Food Delivery logo"
           width={88}
           height={44}
-          className="object-contain"/>
-            </div>
-            <div className="flex gap-4 mx-auto items-center">
-                 <div className="flex w-[251px] h-[36px] gap-4 rounded-[16px] text-xs justify-center items-center content-center bg-amber-50">
-                    <p className="flex text-red-500 gap-2"><MapPin className="w-[20px] h-[20px]"/>Delivery address:</p>
-                    <p className="flex text-gray-500 gap-2">Add location<ChevronRight className="w-[20px] h-[20px]"/></p>
-                </div>
-                <div className="bg-amber-50 w-[36px] h-[36px] flex items-center justify-center rounded-full"><ShoppingCart className="w-[16px] h-[16px]" /></div>
-                <div className="bg-[#EF4444] w-[36px] h-[36px] flex items-center justify-center rounded-full text-amber-50"><User className="w-[16px] h-[16px]" /></div>
-              
-            </div>
-           
+          className="object-contain"
+        />
+      </div>
+      <div className="flex gap-4 mx-auto items-center">
+        <div className="flex w-[251px] h-[36px] gap-4 rounded-[16px] text-xs justify-center items-center content-center bg-amber-50">
+          <p className="flex text-red-500 gap-2">
+            <MapPin className="w-[20px] h-[20px]" />
+            Delivery address:
+          </p>
+          <p className="flex text-gray-500 gap-2">
+            Add location
+            <ChevronRight className="w-[20px] h-[20px]" />
+          </p>
         </div>
-    )
-} 
+        <div className="bg-amber-50 w-[36px] h-[36px] flex items-center justify-center rounded-full">
+          <ShoppingCart className="w-[16px] h-[16px]" />
+        </div>
+        <div className="bg-[#EF4444] w-[36px] h-[36px] flex items-center justify-center rounded-full text-amber-50">
+          <Button
+            className="bg-[#EF4444] w-[36px] h-[36px] flex items-center justify-center rounded-full text-amber-50"
+            type="button"
+            onClick={handleLogout}
+          >
+            <User className="w-[16px] h-[16px]" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,29 +3,29 @@
 import { useRouter } from "next/navigation";
 import { useAuth } from "./UserProvider";
 import { Button } from "@/components/ui/button";
-import  NavPage  from "./Nav/page";
+import NavPage from "./Nav/page";
 
 const Home = () => {
   const { user, setUser } = useAuth();
   const router = useRouter();
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setUser(null); 
-    router.push("/LogIn");
-  };
+  // const handleLogout = () => {
+  //   localStorage.removeItem("token");
+  //   setUser(null);
+  //   router.push("/LogIn");
+  // };
 
   return (
     <div className="text-2xl">
       <h1>Welcome {user ? user.userId : "Guest"}!</h1>
       <NavPage />
 
-      <Button
+      {/* <Button
         className="mt-4 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
         onClick={handleLogout}
       >
         Log out
-      </Button>
+      </Button> */}
 
       <Button
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
