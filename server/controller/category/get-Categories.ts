7cc4e8656a42b10f05e4e577ba48dc3af37e941b
baabar02 <baabar02githu.com>
2025-06-28 15:03:58 +0800

    sad

--- server/controller/category/get-Categories.ts
+++ server/controller/category/get-Categories.ts
@@ -1,32 +1,15 @@
 import { Request, Response } from "express";
 import { FoodCategoryModel } from "../../model/categoryModel";
 
-
-export const allCategory = async (request: Request, response: Response) => {
-  const { categoryName } = request.body;
-  console.log(categoryName,'categories');
-
-  if (!categoryName || typeof categoryName !== "string" || categoryName.trim() === "") {
-    return response.status(400).json({ message: "Valid category name is required" });
-  }
-
-
+export const allCategory = async (_request: Request, response: Response) => {
   try {
-    const existing = await FoodCategoryModel.findOne({ categoryName });
-    if (existing) {
-      return response.status(409).json({ message: "Category already exists" });
-    }
- 
-    const newCategory = await FoodCategoryModel.create({ categoryName });
-    response.status(201).send({
+    const newCategory = await FoodCategoryModel.find({});
+
+    response.status(200).send({
       message: "Category created successfully",
       data: newCategory,
     });
-     console.log(newCategory, "saved");
-    
   } catch (error) {
-  
-    console.error("Error creating category:", error);
     response.status(500).json({ message: "Database connection has error" });
   }
 };
